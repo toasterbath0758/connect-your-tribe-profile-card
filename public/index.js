c409d8aebe46c4eec1838ae002ef473da6e23cee
toasterbath0758 <toasterbath0758@gmail.com>
2026-02-06 09:32:06 +0100

    textures van de lever added#3

--- public/index.js
+++ public/index.js
@@ -1,3 +1,9 @@
+const minecraftLever = document.querySelector('#lever');
+
+minecraftLever.addEventListener('click', function(){
+  minecraftLever.classList.toggle('active');
+})
+
 const leverImg = document.querySelector('.lever-img');
 
 leverImg.addEventListener('click', function(){
